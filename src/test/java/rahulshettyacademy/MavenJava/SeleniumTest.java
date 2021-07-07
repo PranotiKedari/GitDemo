46763337fc642f00ee8b0c8cd00aa70e2b2ca2db
PranotiKedari <pranotiskedari@gmail.com>
2021-07-07 13:46:44 +0530

    First Commit

---
+++ src/test/java/rahulshettyacademy/MavenJava/SeleniumTest.java
@@ -0,0 +1,21 @@
+package rahulshettyacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	
+	//Testing xml file from maven.
+	//How to execute all test cases from Test Folder - mvn test.
+	public void BroswerAutomation()
+	{
+		System.out.println("BroswerAutomation");
+		}
+	
+	@Test
+	public void ElementsUI()
+	{
+		System.out.println("ElementsUI");
+	}
+	
+}
